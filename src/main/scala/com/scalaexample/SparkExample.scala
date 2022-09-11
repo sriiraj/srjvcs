5ee792b0c028b1e55026d2659b53d478d82a0999
sriiraj <sriiraj.jaami12@gmail.com>
2022-09-11 21:38:40 +0530

    First Commit

---
+++ src/main/scala/com/scalaexample/SparkExample.scala
@@ -0,0 +1,13 @@
+package com.scalaexample
+
+/**
+ * @author ${user.name}
+ */
+object SparkExample{
+    
+  def main(args : Array[String]):Unit = {
+    println( "Hello World!" )
+    // println("concat arguments = " + foo(args))
+  }
+
+}
